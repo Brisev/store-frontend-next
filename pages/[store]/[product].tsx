2b148402af93b296fdccaa2ad1c486825de502d0
Bright Ikhide <ikhidebright@gmail.com>
2022-11-24 09:55:06 +0100

    fixes

--- pages/[store]/[product].tsx
+++ pages/[store]/[product].tsx
@@ -31,6 +31,24 @@ const Item = styled(Paper)(({ theme }) => ({
 }));
 
 const ProductDetails = () => {
+  const triggerShare = () => {
+    console.log("kkkkkkkk");
+    // alert("kkk");
+
+    // if (navigator.share) {
+    navigator
+      .share({
+        title: "WebShare API Demo",
+        url: "https://codepen.io/ayoisaiah/pen/YbNazJ",
+      })
+      .then(() => {
+        console.log("Thanks for sharing!");
+      })
+      .catch(console.error);
+    // } else {
+    // fallback
+    // }
+  };
   return (
     <Fragment>
       <Grid container spacing={2}>
@@ -63,7 +81,12 @@ const ProductDetails = () => {
               </div>
 
               <div>
-                <IconButton aria-label="share" sx={{ mt: -0.5 }} size="small">
+                <IconButton
+                  onClick={triggerShare}
+                  aria-label="share"
+                  sx={{ mt: -0.5 }}
+                  size="small"
+                >
                   <Share fontSize="small" />
                 </IconButton>
 
