af1d9ef77637ceb07a96d845ace0fc4f2118f8ca
Isaiah Ernest Ovie <officialisaiahovie@gmail.com>
2023-01-04 21:32:45 +0100

    fix position missing dependency error in AddressBookItem component

--- pages/[store]/address-book/index.tsx
+++ pages/[store]/address-book/index.tsx
@@ -1,13 +1,13 @@
-import { Button, Grid, Typography } from "@mui/material";
-import { Stack } from "@mui/system";
-import { Fragment, ReactElement, useState } from "react";
+import { Button, Grid, Typography } from '@mui/material';
+import { Stack } from '@mui/system';
+import { Fragment, ReactElement, useState } from 'react';
 // import { Grid } from "swiper";
-import AddressBookItem from "../../../components/AddressBook/AddressBookItem";
-import Modal from "../../../components/Misc/Modal";
-import HorizontalQuickProductList from "../../../components/Product/HorizontalQuickProductList";
-import StoreLayout from "../../../layouts/store";
-import DUMMY_JSON from "../../../dummy/products.json";
-import AddressBookCreateAndUpdate from "../../../components/AddressBook/AddressBookCreateAndUpdate";
+import AddressBookItem from '../../../components/AddressBook/AddressBookItem';
+import Modal from '../../../components/Misc/Modal';
+import HorizontalQuickProductList from '../../../components/Product/HorizontalQuickProductList';
+import StoreLayout from '../../../layouts/store';
+import DUMMY_JSON from '../../../dummy/products.json';
+import AddressBookCreateAndUpdate from '../../../components/AddressBook/AddressBookCreateAndUpdate';
 
 const AddressBook = () => {
   const [openDeleteModal, setOpenDeleteModal] = useState(false);
@@ -26,9 +26,9 @@ const AddressBook = () => {
       <Modal
         open={openAddAddressModal}
         onHandleCloseModal={handleAddAddressModal}
-        cancelText="Cancel"
-        okText="Add"
-        header="Add Address"
+        cancelText='Cancel'
+        okText='Add'
+        header='Add Address'
       >
         <AddressBookCreateAndUpdate />
       </Modal>
@@ -36,15 +36,15 @@ const AddressBook = () => {
         open={openDeleteModal}
         onHandleCloseModal={onDeleteAddress}
         cancelText="Don't Delete"
-        okText="Yes, Delete"
-        header="Delete Address"
+        okText='Yes, Delete'
+        header='Delete Address'
       >
         <Typography fontSize={15}>
           Are you sure want to remove this Address? This cannot be reversed
         </Typography>
       </Modal>
       <Typography
-        variant="h5"
+        variant='h5'
         sx={{
           fontWeight: 700,
           // pb: "2rem",
@@ -54,9 +54,9 @@ const AddressBook = () => {
       </Typography>
 
       <Stack
-        display="flex"
-        flexDirection="row"
-        justifyContent="end"
+        display='flex'
+        flexDirection='row'
+        justifyContent='end'
         mb={1}
         // mt={1}
       >
@@ -68,22 +68,25 @@ const AddressBook = () => {
         rowSpacing={{ xs: 0, sm: 6, md: 8 }}
         columnSpacing={{ xs: 0, sm: 2, md: 1 }}
         columns={{ xs: 4, sm: 6, md: 8, lg: 12 }}
-        mx="auto"
+        mx='auto'
         mb={8}
       >
         {[1, 2, 3].map((address, index) => {
           return (
             <Grid item xs={12} sm={3} md={3} lg={4} key={index}>
-              <AddressBookItem onDeleteAddress={onDeleteAddress} />
+              <AddressBookItem
+                onDeleteAddress={onDeleteAddress}
+                position={index + 1}
+              />
             </Grid>
           );
         })}
       </Grid>
 
       <HorizontalQuickProductList
-        title="Recently seen"
+        title='Recently seen'
         related={DUMMY_JSON}
-        seeMoreLink="/store/saved"
+        seeMoreLink='/store/saved'
       />
     </Fragment>
   );
